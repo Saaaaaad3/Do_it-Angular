b443857d6e7b4b68a43805df85e48b9f7ccef3cd
SaadMansuri3 <saadmansuri.work@gmail.com>
2024-12-17 06:35:45 +0530

    Initial Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./features/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'boards',
+    loadChildren: () =>
+      import('./features/boards/boards.module').then((m) => m.BoardsModule),
+  },
+  {
+    path: 'cards',
+    loadChildren: () =>
+      import('./features/cards/cards.module').then((m) => m.CardsModule),
+  },
+  {
+    path: 'lists',
+    loadChildren: () =>
+      import('./features/lists/lists.module').then((m) => m.ListsModule),
+  },
+  {
+    path: 'user',
+    loadChildren: () =>
+      import('./features/user/user.module').then((m) => m.UserModule),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/features/boards/boards.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boards',
+  standalone: false,
+  
+  templateUrl: './boards.component.html',
+  styleUrl: './boards.component.css'
+})
+export class BoardsComponent {
+
+}
