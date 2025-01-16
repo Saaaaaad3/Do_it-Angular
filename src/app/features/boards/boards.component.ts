130e7978273e9f280d709b530141270a12b36e40
SaadMansuri3 <saadmansuri.work@gmail.com>
2025-01-16 07:24:03 +0530

    Fixes and Dynamic Data For Boards

--- src/app/features/boards/boards.component.ts
+++ src/app/features/boards/boards.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { BoardsService } from './services/boards.service';
+import { HttpClient } from '@angular/common/http';
 
 interface Board {
   id: number;
@@ -17,24 +19,33 @@ interface Board {
 export class BoardsComponent implements OnInit {
   boards: Board[] = [];
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private boardsService: BoardsService) {}
 
   ngOnInit(): void {
-    this.boards = [
-      { id: 1, title: 'Angular' },
-      { id: 2, title: 'Dot Net Core' },
-      { id: 3, title: 'CICD' },
-      { id: 4, title: 'Azure' },
-      { id: 5, title: 'AI Features!' },
-    ];
+    // this.boards = [
+    //   { id: 1, title: 'Angular' },
+    //   { id: 2, title: 'Dot Net Core' },
+    //   { id: 3, title: 'CICD' },
+    //   { id: 4, title: 'Azure' },
+    //   { id: 5, title: 'AI Features!' },
+    // ];
+    this.boardsService.getAllBoards().subscribe((data: Board[]) => {
+      this.boards = data;
+      console.log(this.boards);
+    });
   }
 
-  addNewBoard(): void {
-    const newId = this.boards.length + 1;
-    this.boards.push({ id: newId, title: `New board ${newId}` });
+  addNewBoard() {
+    const boardName = prompt('Enter the board name');
+    const newBoard: Board = { id: 1, title: boardName || 'New Board' };
+    this.boardsService.AddNewBoard(newBoard).subscribe();
   }
 
   viewBoard(boardId: number): void {
     this.router.navigate(['/boards', boardId]);
+  }
+
+  RemoveBoard(boardId: Number) {
+    this.boardsService.RemoveBoard(boardId).subscribe();
   }
 }
---
+++ src/app/features/boards/services/boards.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, OnInit } from '@angular/core';
+import { map, Observable } from 'rxjs';
+
+interface Board {
+  id: number;
+  title: string;
+}
+
+interface AddBoard {
+  UserId: number;
+  BoardName: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BoardsService {
+  constructor(private httpClient: HttpClient) {}
+
+  boards: Board[] = [];
+
+  getAllBoards(): Observable<Board[]> {
+    return this.httpClient
+      .get<Board[]>(
+        'https://localhost:44318/api/Boards/GetAllBoardsByUserId?UserId=1'
+      )
+      .pipe(
+        map((response: any) => {
+          return response.map((item: any) => ({
+            id: item.boardId,
+            title: item.boardName,
+          }));
+        })
+      );
+  }
+
+  AddNewBoard(board: Board): Observable<Board> {
+    return this.httpClient.post<Board>(
+      'https://localhost:44318/api/Boards/AddNewBoard',
+      { UserId: board.id, BoardName: board.title },
+      { headers: { 'Content-Type': 'application/json' } }
+    );
+  }
+
+  RemoveBoard(boardId: Number) {
+    return this.httpClient.delete(
+      'https://localhost:44318/api/Boards/RemoveBoard?boardId=' + boardId
+    );
+  }
+}
